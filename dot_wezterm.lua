99d0f7f93b4d80bfebe35b6df7be537eee91c4aa
Viktor Holmgren <viktor.holmgren@gmail.com>
2024-11-18 21:16:48 +0100

    Add basic zsh, wezterm and nvim configs

---
+++ dot_wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("Cascadia Mono NF")
+config.font_size = 14
+
+config.color_scheme = "Tokyo Night"
+
+config.enable_tab_bar = true
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+
+-- and finally, return the configuration to wezterm
+return config
